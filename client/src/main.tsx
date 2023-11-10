e22f72e60afd1e0bef314f54dec62832bcf0142c
Taavetti Kukkonen <taavetti.kukkonen@taitounited.fi>
2023-11-10 16:57:02 +0200

    chore: setup linter

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<div>Hello World</div>, document.getElementById('app')!);
+ReactDOM.render(<div>Hello Worl</div>, document.getElementById('app')!)
