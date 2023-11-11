4d52648cc46f4a5f6b34a524554a7f1801e767bd
Taavetti Kukkonen <taavetti.kukkonen@taitounited.fi>
2023-11-11 11:39:09 +0200

    feat: phone background

--- client/src/components/PageLayout.tsx
+++ client/src/components/PageLayout.tsx
@@ -1,38 +1,56 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useRef } from 'react';
 import styled from 'styled-components';
 import { Text } from './Text';
 import logo from '../../assets/painload.png';
 import { LampIcon } from '../common/icons';
 import { IconType } from 'react-icons/lib';
 import { Link } from '@tanstack/react-router';
+import phone from '../../assets/phone.png';
+
+const PHONE_DIMENSIONS = {
+  width: 1640,
+  height: 3540,
+};
+
+const ASPECT = PHONE_DIMENSIONS.width / PHONE_DIMENSIONS.height;
 
 export function PageLayout({ children }: { children: ReactNode }) {
+  const phoneRef = useRef<HTMLImageElement>(null);
+
+  const containerHeight = (phoneRef.current?.height || 0) - 90;
+  const containerWidth = containerHeight * ASPECT;
+
   return (
-    <Container>
-      <TopBar>
-        <img width={150} src={logo} style={{ objectFit: 'contain' }}></img>
-      </TopBar>
+    <OuterWrapper>
+      <Wrapper>
+        <Phone src={phone} ref={phoneRef}></Phone>
+        <Container style={{ height: containerHeight, width: containerWidth }}>
+          <TopBar>
+            <img width={150} src={logo} style={{ objectFit: 'contain' }}></img>
+          </TopBar>
 
-      <Scroller id="scroller">{children}</Scroller>
+          <Scroller id="scroller">{children}</Scroller>
 
-      {/* Place to render stuff outside the scroller */}
-      <div id="context"></div>
+          {/* Place to render stuff outside the scroller */}
+          <div id="context"></div>
 
-      <BottomNav>
-        <BottomMenuItem link="/" icon={LampIcon}>
-          Chat
-        </BottomMenuItem>
-        <BottomMenuItem link="/data" icon={LampIcon}>
-          Your data
-        </BottomMenuItem>
-        <BottomMenuItem link="/device" icon={LampIcon}>
-          Device
-        </BottomMenuItem>
-        <BottomMenuItem link="/account" icon={LampIcon}>
-          Account
-        </BottomMenuItem>
-      </BottomNav>
-    </Container>
+          <BottomNav>
+            <BottomMenuItem link="/" icon={LampIcon}>
+              Chat
+            </BottomMenuItem>
+            <BottomMenuItem link="/data" icon={LampIcon}>
+              Your data
+            </BottomMenuItem>
+            <BottomMenuItem link="/device" icon={LampIcon}>
+              Device
+            </BottomMenuItem>
+            <BottomMenuItem link="/account" icon={LampIcon}>
+              Account
+            </BottomMenuItem>
+          </BottomNav>
+        </Container>
+      </Wrapper>
+    </OuterWrapper>
   );
 }
 
@@ -71,11 +89,30 @@ const BottomNavLink = styled(Link)`
   width: 72px;
 `;
 
+const Phone = styled.img`
+  height: 100vh;
+  object-fit: contain;
+  position: absolute;
+  pointer-events: none;
+`;
+
+const Wrapper = styled.div``;
+
+const OuterWrapper = styled.div`
+  width: 100vw;
+  justify-content: center;
+  display: flex;
+`;
+
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100vh;
-  width: 100vw;
+  width: 330px;
+  border-radius: 32px;
+  margin-left: 18px;
+  margin-top: 32px;
+  overflow: hidden;
+  background-color: white;
 `;
 
 const Scroller = styled.div`
